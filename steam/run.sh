70de317004a3a6bbdafc647a46d4b8b838cd3260
Max Mruszczak <u@1ux.org>
2023-08-27 00:24:39 +0000

    Change steam image base to archlinux

--- steam/run.sh
+++ steam/run.sh
@@ -1,9 +1,13 @@
 docker run -it --rm \
 	-e DISPLAY=$DISPLAY \
+	--ulimit nofile=524288:524288 \
+	--ulimit core=-1 \
+	--ulimit data=-1 \
+	--ulimit fsize=-1 \
 	-v /tmp/.X11-unix:/tmp/.X11-unix \
 	-v /mnt/aux/steam:/home/steam \
 	-v /var/run/dbus/system_bus_socket:/var/run/dbus/system_bus_socket \
 	--privileged \
 	--network host \
 	--ipc=host \
-	steam:latest
+	steam:latest steam -no-cef-sandbox
