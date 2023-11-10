910658166056987726d48b9ce1273079cb73ca54
Max Mruszczak <u@1ux.org>
2023-11-10 18:38:24 +0000

    Add user to input group in steam port

--- steam/entrypoint.sh
+++ steam/entrypoint.sh
@@ -3,7 +3,7 @@
 if [ -n "$USER_ID" ]
 then
 	useradd -M -s /bin/bash -u "$USER_ID" -o -d /home/user user
-	usermod -aG wheel,audio,video user
+	usermod -aG wheel,audio,video,input user
 	[ -n "$GROUPS" ] && usermod -aG "$GROUPS" user
 	chown -R user:user /home/user
 	exec doas -u user "$@"
